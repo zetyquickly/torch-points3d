0aa4b55615c9936a2d1a5acef9c70aed54dddcab
emil <emilzq@bk.ru>
2020-10-20 13:37:36 +0000

    Fix variable names and assignings

--- torch_points3d/datasets/panoptic/utils.py
+++ torch_points3d/datasets/panoptic/utils.py
@@ -9,7 +9,7 @@ def set_extra_labels(data, instance_classes, num_max_objects):
     - vote_label: [num_points, 3] displacement of bbox center with respect to the object points
     - instance_mask: [num_points] boolean mask
     - object_center_label: [num_max_objects, 3] one per instance mean position vector
-    - object_vote_label: [num_points, 3] displacement of each object point with respect to the object center
+    - object_displmnt_mask: [num_points, 3] displacement of each object point with respect to the object center
     """
     # Initaliase variables
     num_points = data.pos.shape[0]
@@ -20,7 +20,7 @@ def set_extra_labels(data, instance_classes, num_max_objects):
     centers = []
     object_centers = []
     point_votes = torch.zeros([num_points, 3])
-    object_point_votes = torch.zeros([num_points, 3])
+    object_point_displmnt = torch.zeros([num_points, 3])
     instance_labels = torch.zeros(num_points, dtype=torch.long)
     instance_idx = 1
     for i_instance in instances:
@@ -36,7 +36,7 @@ def set_extra_labels(data, instance_classes, num_max_objects):
             point_votes[ind, :] = center - pos
             centers.append(torch.tensor(center))
             object_center = pos.mean(axis=0)
-            object_point_votes[ind, :] = pos - object_center
+            object_point_displmnt[ind, :] = pos - object_center
             object_centers.append(torch.tensor(object_center))
             instance_labels[ind] = instance_idx
             instance_idx += 1
@@ -52,7 +52,7 @@ def set_extra_labels(data, instance_classes, num_max_objects):
         data.object_center_label[:num_instances, :] = torch.stack(object_centers)
 
     data.vote_label = point_votes.float()
-    data.object_point_votes = object_point_votes.float()
+    data.object_displmnt_mask = object_point_displmnt.float()
     data.instance_labels = instance_labels
     data.instance_mask = instance_labels != 0
     data.num_instances = torch.tensor([num_instances])
